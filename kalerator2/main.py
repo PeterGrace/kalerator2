17b47404ed5b33d2726c6f3bd93ba05c0ae392f0
Nextraztus <relay@nextraztus.com>
2016-11-19 16:21:47 -0700

    additional conversion to cli tool

--- kalerator2/main.py
+++ kalerator2/main.py
@@ -1,21 +1,47 @@
-"""The primary module for this skeleton application."""
-import click
-from gevent.wsgi import WSGIServer
-from werkzeug.serving import run_with_reloader
-from kalerator.web.app import app
+import argparse
+import json
+import os
+import sys
 
+from kalerator.keyboard import Keyboard
 
-def run_server():
-    app.debug = True
-    ws = WSGIServer(listener=('0.0.0.0', 6543), application=app)
-    print(' * Listening on http://0.0.0.0:6543/')
-    ws.serve_forever()
+def write_file(filename, data):
+    with open(filename, 'wb') as f:
+        f.write(data)
 
+def main(filename, flag_free=False, flag_paid=False):
+    with open(filename) as file:
+        data = json.load(file)
 
+    (root, ext) = os.path.splitext(filename)
+   
+    if flag_free:
+        free_board_fn = '{0}{1}.{2}'.format(root, '-board-free', '.scr')
+        free_schm_fn = '{0}{1}.{2}'.format(root, '-schm-free', '.scr')
+        k_free = Keyboard(filename, flag_free)
+        write_file(free_board_fn, k_free.board_scr())
+        write_file(free_schm_fn, k_free.schematic_scr())
 
-@click.command()
-def main():
-    run_server()
+    if flag_paid:
+        paid_board_fn = '{0}{1}.{2}'.format(root, '-board-paid', '.scr')
+        paid_schm_fn = '{0}{1}.{2}'.format(root, '-schm-paid', '.scr')
+        k_paid = Keyboard(filename, flag_paid)
+        write_file(paid_board_fn, k_paid.board_scr())
+        write_file(paid_schm_fn, k_paid.schematic_scr())
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('free', action='store_true', default=False)
+    parser.add_argument('paid', action='store_true', default=False)
+    args = parser.parse_args()
+
+    if not args.filename:
+        print('Require --filename for input json; output files will be (rootname).json -> (rootname)-{board|schm}-{free|paid}.scr')
+        sys.exit(1)
+
+    if not (args.free or args.paid):
+        print('Was given nothing to do, require --free or --paid.')
+        sys.exit(1)
+
+    sys.exit(main(args.filename, args.free, args.paid))
